8902bdb3e2edb88f4892138306ea71e0481332b7
sancty <sanctifieryawmensah55@gmail.com>
2024-04-03 23:16:44 +0200

    Ajout commentaire

--- Todo_List.js
+++ Todo_List.js
@@ -1,136 +1,115 @@
+// Création d'un module auto-appelant pour encapsuler la logique
+let gestionnaireTache = (function() {
+    // Déclaration du compteur pour le nombre total de tâches
+    let nombreLi = 0;
 
-// creation d'un module auto appelant pour l'encapsulation de la logique 
-let gestionnaireTache =(function(){
-
-   // declaration du compteur 
-    let nombreLi = 0 ;
-
-    function incrementationCompteur(){
+    // Fonction pour incrémenter le compteur
+    function incrementationCompteur() {
         nombreLi++;
+        // Met à jour l'affichage du nombre total de tâches
         document.querySelector(".nombreLi").textContent = nombreLi;
     }
 
-    function decrementationCompteur(){
+    // Fonction pour décrémenter le compteur
+    function decrementationCompteur() {
         nombreLi--;
+        // Met à jour l'affichage du nombre total de tâches
         document.querySelector(".nombreLi").textContent = nombreLi;
     }
 
-    return   {
+    // Retourne les fonctions à utiliser en dehors du module
+    return {
         incrementationCompteur,
         decrementationCompteur
     };
 })();
 
+// Déclaration d'une variable pour le nombre total de tâches
+let nombreLi = 0;
 
-
-
-
-// declaration du compteur 
-
-let nombreLi = 0 ;
-
-
-const inputchamp = document.querySelector('#nomTache');   
+// Sélection des éléments HTML nécessaires
+const inputchamp = document.querySelector('#nomTache');
 const ouputchamp = document.querySelector('.list-group');
 
-
-function creationBouttonSupprimer(){
-
+// Fonction pour créer un bouton de suppression
+function creationBouttonSupprimer() {
     const boutonSupprimer = document.createElement('button');
-    boutonSupprimer.className ='ms-auto btn btn-danger btn-sm';
-    boutonSupprimer.innerHTML ='<i class="bi-trash"></i>';
-
-    return boutonSupprimer ;
+    boutonSupprimer.className = 'ms-auto btn btn-danger btn-sm';
+    boutonSupprimer.innerHTML = '<i class="bi-trash"></i>';
+    return boutonSupprimer;
 }
 
-function actionButtonSupprimer(div){
-    div.style.transform = 'translateY(-150px)'; 
-    div.style.position   = 'relative';
+// Fonction pour effectuer l'action de suppression d'un élément
+function actionButtonSupprimer(div) {
+    // Animation de translation vers le haut avant la suppression
+    div.style.transform = 'translateY(-150px)';
+    div.style.position = 'relative';
     div.style.transition = 'transform 1s';
 
+    // Suppression de l'élément du DOM après la fin de l'animation
     setTimeout(function() {
-
-        div.remove(); // Supprimer l'élément du DOM après que la transition d'opacité soit terminée
-
-        // décrementation du nombre total de li 
+        div.remove();
+        // Décrémentation du nombre total de tâches
         gestionnaireTache.decrementationCompteur();
-
     }, 1000);
-
 }
 
-// fonction modifier 
-
+// Fonction pour créer un bouton de modification
 function creationBouttonModifier() {
-
     const boutonModifier = document.createElement('button');
     boutonModifier.className = 'btn btn-info ms-auto'; // Ajout de la classe ms-2 pour décalage à gauche
     boutonModifier.innerHTML = '<i class="fas fa-pencil-alt"></i> Modifier';
 
-    // style sur le button 
-
+    // Style sur le bouton
     boutonModifier.style.color = '#fff';
     boutonModifier.style.backgroundColor = '#17a2b8';
     boutonModifier.style.borderColor = '#17a2b8';
-    //boutonModifier.style.padding = '5px 10px';
     boutonModifier.style.borderRadius = '5px';
-
 
     return boutonModifier;
 }
 
-// ajout dans la todo_list 
-
-function bouttonAjouter(){ 
-
-    // creation d'une nouvlle li 
+// Fonction pour ajouter un élément à la todo list
+function bouttonAjouter() {
+    // Création d'une nouvelle li
     const li = document.createElement('li');
-    li.classList ='todo list-group-item d-flex align-items-center';
+    li.classList = 'todo list-group-item d-flex align-items-center';
 
-    // incrémentation des Li : le nombre total de li
+    // Incrémentation du nombre total de tâches
     gestionnaireTache.incrementationCompteur();
 
-
-    // creation du label
+    // Création du label
     const label = document.createElement("label");
-    label.className ='ms-2 form-check-label';
-    label.setAttribute('for' , 'todo-' + (ouputchamp.children.length + 1))
+    label.className = 'ms-2 form-check-label';
+    label.setAttribute('for', 'todo-' + (ouputchamp.children.length + 1));
 
-    // creation du inputCheckbox
-
+    // Création de l'input Checkbox
     const inputCheckbox = document.createElement("input");
     inputCheckbox.classList = 'form-check-input';
     inputCheckbox.id = 'todo-' + (ouputchamp.children.length + 1);
     inputCheckbox.type = 'checkbox';
-    
-    // boutton suppression 
-    const boutonSupprimer = creationBouttonSupprimer();
 
-    boutonSupprimer.addEventListener('click',function(){
+    // Création du bouton de suppression
+    const boutonSupprimer = creationBouttonSupprimer();
+    boutonSupprimer.addEventListener('click', function() {
         actionButtonSupprimer(li);
     });
 
-   
-
-
+    // Récupération de la valeur saisie dans le champ
     const inputchampvalue = inputchamp.value;
 
-    if(inputchampvalue !=="" ){
-        // valeur dans le champs dans le nouveau contenu crée
+    // Vérification de la saisie
+    if (inputchampvalue !== "") {
+        // Affichage de la valeur dans le label de la nouvelle tâche
         label.textContent = inputchampvalue.trim();
 
-        // l'ajout dans ul 
+        // Ajout de la nouvelle tâche à la liste
         ouputchamp.appendChild(li);
-        
         li.appendChild(inputCheckbox);
         li.appendChild(label);
-     
-        //li.appendChild(creationBouttonModifier());
         li.appendChild(boutonSupprimer);
-        //li.appendChild(boutonSupprimer);
 
+        // Réinitialisation du champ de saisie
         inputchamp.value = "";
     }
-    
 }
-
